70945d9eb6dbab014ae5e109e5649a66806b01c5
shashwat shukla <shuklashashwat009@gmail.com>
2025-07-29 00:00:11 +0530

    shadcn install

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return(
     <div>
-        hello
+        hello <Button>
+             it is a button
+        </Button>
     </div>
   )
 }
